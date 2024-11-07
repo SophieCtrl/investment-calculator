b4c4061186bb07b0f13b5f089715585c683cf221
Sophie <sallysophie@hotmail.de>
2024-11-07 15:19:45 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// components: header, user input, table
+// store user input
+// derive investment results using provided utility functions
+// display results in an HTML table
+// display info message if input is invalid
+
+function App() {
+  return <p></p>;
+}
+
+export default App;
